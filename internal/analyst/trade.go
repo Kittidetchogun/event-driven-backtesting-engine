e6388453341258bab87cee7f5677157378a8a7e0
fah <66070021@kmitl.ac.th>
2026-08-01 15:21:28 +0700

    trade stat

--- internal/analyst/trade.go
+++ internal/analyst/trade.go
@@ -12,6 +12,9 @@ type TradeStat struct {
 	TotalTrades int
 	NetProfit   float64
 	Winrate     float64
+	ProfitableTrades int
+	Loss_Avg float64
+	Win_Avg float64
 }
 
 // func NewTrade(initCapital float64) *TradeStat {
@@ -28,3 +31,57 @@ type TradeStat struct {
 // 	}
 // 	return total
 // }
+
+func winrate(trades []domain.Trade) float64 {
+	if len(trades) == 0 {
+		return 0.0
+	} else {
+		win := 0
+		for _, t := range trades {
+			if domain.Profit(t) > 0 {
+				win++
+			}
+		}
+		return float64(win) / float64(len(trades))
+	}
+}
+
+func profitTrades(trades []domain.Trade) int {
+	profit := 0
+	for _, t := range trades {
+		if domain.Profit(t) > 0 {
+			profit++
+		}
+	}
+	return profit
+}	
+
+func lossAvg(trades []domain.Trade) float64 {
+	Loss := 0.0
+	LossCount := 0
+	for _, t := range trades {
+		if domain.Profit(t) < 0 {
+			Loss += domain.Profit(t)
+			LossCount++
+		}
+	}
+	if LossCount == 0 {
+		return 0.0
+	}
+	return Loss / float64(LossCount)
+}
+
+func winAvg(trades []domain.Trade) float64 {
+	Win := 0.0
+	WinCount := 0
+	for _, t := range trades {
+		if domain.Profit(t) > 0 {
+			Win += domain.Profit(t)
+			WinCount++
+		}
+	}
+	if WinCount == 0 {
+		return 0.0
+	}
+	return Win / float64(WinCount)
+}
